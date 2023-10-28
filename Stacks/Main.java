c1e2b32b7e48f127b8b4de0ff3c55d1546e31f9d
Suyash <smhj022@gmail.com>
2023-10-28 20:46:18 +0530

    Stacks: Added constructor and method to pop and push Node from a stack

---
+++ Stacks/Main.java
@@ -0,0 +1,20 @@
+package Stacks;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Stack stack = new Stack(3);
+        stack.push(2);
+        stack.push(1);
+
+        stack.printStack();
+
+        System.out.println(stack.pop().value); // 1
+        System.out.println(stack.pop().value); // 2
+        System.out.println(stack.pop().value); // 3
+        System.out.println(stack.pop()); // null
+
+        stack.printStack();
+
+    }
+}
