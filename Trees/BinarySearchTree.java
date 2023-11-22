042f127449599b2fefaa4ba0f6dec575b7234c86
Suyash <smhj022@gmail.com>
2023-11-22 19:25:41 +0530

    Trees: Added constrator for tree

---
+++ Trees/BinarySearchTree.java
@@ -0,0 +1,19 @@
+package Trees;
+
+public class BinarySearchTree {
+    Node root;
+
+    class Node{
+
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+    }
+
+
+}
