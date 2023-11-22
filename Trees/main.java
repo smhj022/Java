c8c2dd5bd04362e030c7bb73e45cbcfb8a2abc59
Suyash <smhj022@gmail.com>
2023-11-22 20:20:56 +0530

    Added method to insert a node in a BST

--- Trees/main.java
+++ Trees/main.java
@@ -7,7 +7,14 @@ public class main {
 
         BinarySearchTree myBST = new BinarySearchTree();
 
-        System.out.println("root = " + myBST.root);
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(82);
 
+        myBST.insert(27);
+        System.out.println(myBST.root.left.right.value);
     }
 }
