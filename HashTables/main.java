fe4b2c5c75af5ca405f250042a76b51dd13d2c7d
Suyash <smhj022@gmail.com>
2023-11-24 20:52:37 +0530

    Added constructor for Hash table and hash function to generate hash

---
+++ HashTables/main.java
@@ -0,0 +1,10 @@
+package HashTables;
+
+public class main {
+    public static void main(String[] args) {
+
+        HashTable myHashTable = new HashTable();
+
+        myHashTable.printTable();
+    }
+}
