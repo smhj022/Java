557521c9c356f4678114a48728925df9f63113fa
Suyash <smhj022@gmail.com>
2023-12-08 21:50:07 +0530

    Added method to add vertex in graph.

---
+++ Graphs/main.java
@@ -0,0 +1,15 @@
+package Graphs;
+
+public class main {
+    public static void main(String[] args) {
+
+        Graph myGraph = new Graph();
+
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+
+        myGraph.printGraph();
+
+
+    }
+}
--- HashTables/main.java
+++ HashTables/main.java
@@ -5,7 +5,6 @@ import java.util.*;
 
 public class main {
     public static void main(String[] args) {
-
         HashTable myHashTable = new HashTable();
     }
 }
