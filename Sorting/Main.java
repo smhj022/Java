60d9da8878c1c25f94b533c5bffe98431fe9645d
Suyash <smhj022@gmail.com>
2024-01-11 20:52:44 +0530

    Added Selection sort method to sort the array

--- Sorting/Main.java
+++ Sorting/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
 
         int[] arr = {999,21,23,41,0,87,543,6,1,2,76,12,0,-1};
-        System.out.println(Arrays.toString(SortingAlgos.bubbleSort(arr)));
+        System.out.println(Arrays.toString(SortingAlgos.selectionSort(arr)));
     }
 }
--- Sorting/SortingAlgos.java
+++ Sorting/SortingAlgos.java
@@ -17,7 +17,23 @@ public class SortingAlgos {
                     arr[j + 1] = temp;
                 }
             }
-            System.out.println(i + " " +Arrays.toString(arr));
+        }
+        return arr;
+    }
+
+    public static int[] selectionSort(int[] arr){
+
+        for (int i = 0; i < arr.length; i++){
+            int minIndex = i;
+            for(int j = i; j< arr.length; j++) {
+                if (arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+            System.out.println(Arrays.toString(arr));
         }
         return arr;
     }
