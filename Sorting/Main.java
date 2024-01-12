a97942fa0f0d2c97cb226bcad07ec9384ace9469
Suyash <smhj022@gmail.com>
2024-01-12 22:00:38 +0530

    Added break statement in the Inserstion sort to make it more effivient

--- Sorting/Main.java
+++ Sorting/Main.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-//        int[] arr = {999,21,23,41,0,87,543,6,1,2,76,12,0,-1};
-        int[] arr = {4,2,6,5,1,3};
+        int[] arr = {999,21,23,41,0,87,543,6,1,2,76,12,0,-1};
+//        int[] arr = {1,2,4,3,5,6};
         System.out.println(Arrays.toString(SortingAlgos.insertionSort(arr)));
     }
 }
--- Sorting/SortingAlgos.java
+++ Sorting/SortingAlgos.java
@@ -47,6 +47,8 @@ public class SortingAlgos {
                     int temp = arr[j];
                     arr[j] = arr[j -1];
                     arr[j-1] = temp;
+                } else{
+                    break;
                 }
             }
         }
